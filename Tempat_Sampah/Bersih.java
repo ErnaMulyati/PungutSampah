be6f5fc537d3cf1310dff10f1a280e90b2cd9ea6
nitawati tulak <nita.tulak@yahoo.com>
2018-05-19 16:12:36 +0800

    perubahan bersih

--- Tempat_Sampah/Bersih.java
+++ Tempat_Sampah/Bersih.java
@@ -17,7 +17,7 @@ public class Bersih extends World
     public Bersih()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1000, 700, 1); 
+        super(1000, 710, 1); 
         
         prepare();
     }
